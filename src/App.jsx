91585ae4a45db063fc207e8580eb7db873dca5cb
jawad ali <jawad705294@gmail.com>
2026-07-28 20:53:47 +0500

    Deleted extra files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './index.css'
 import Sidebar from './components/Sidebar'
+import Sidebartest from './components/Sidebartest'
 import { Routes } from 'react-router-dom'
 import { Route } from 'react-router-dom'
 import Home from './Pages/Home'
@@ -10,11 +11,12 @@ import Contact from './Pages/Contact'
 
 const App = () => {
   return (
-    <div className='bg-[#060f0b] min-h-screen' >
-      <div className='flex w-full'>
+    <div className="min-h-screen bg-[#060f0b]">
+      <div className="flex w-full">
         <Sidebar />
+        {/* <Sidebartest /> */}
         {/* ml-[max(5vw,25px)] */}
-        <div className='mx-10 text-black text-base ml-[23%]' >
+        <div className="mx-10 text-base text-black md:ml-[23%]">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/services" element={<Services />} />
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,32 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
-import { FaDownload } from "react-icons/fa";
-import manimg from "../assets/manImage.png"
+import { FaDownload } from 'react-icons/fa'
+import manimg from '../assets/manImage.png'
+import { LuSquareMenu } from 'react-icons/lu'
+import { FaRegWindowClose } from 'react-icons/fa'
+
+// <FaRegWindowClose  className='text-white z-100 right-0'/>  cross
 
 const Sidebar = () => {
-    return (
-        <div className='w-[50%] z-11  sm:w-[22%] fixed top-0 left-0  hidden h-screen bg-[#002512] md:flex flex-col justify-between  '>
-       
-            <div className='p-8'>
-                <div className='   flex items-center justify-center '><img src={manimg} className='w-20' alt="" /></div>
-                <div className='flex flex-col gap-4 pt-10 place-items-center uppercase text-white'>
-                    <NavLink to='/' className='flex items-center gap-3 px-6 py-3' >
-                        <p className='text-lg'>Home</p>
-                    </NavLink>
-                    <NavLink to='/services' className='flex items-center gap-3 px-6 py-3'>
-                        <p className='text-lg'>Services</p>
-                    </NavLink>
-                    <NavLink to='/projects' className='flex items-center gap-3 px-6 py-3'>
-                        <p className='text-lg'>Projects</p>
-                    </NavLink>
-                    <NavLink to='/contact' className='flex items-center gap-3 px-6 py-3'>
-                        <p className='text-lg'>Contact</p>
-                    </NavLink>
-                </div>
+  const [toggle, setToggle] = useState(false)
+  console.log(toggle)
+  return (
+    <div>
+      <button
+        onClick={() => setToggle(!toggle)}
+        className="fixed top-4 left-4 z-100 flex cursor-pointer text-2xl text-white md:hidden"
+      >
+        {toggle ? <FaRegWindowClose /> : <LuSquareMenu />}
+      </button>
+
+      <div>
+        {
+          <div
+            className={`fixed top-0 left-0 z-11 ${toggle ? 'traslate-x-0' : '-translate-x-full'} h-screen w-[50%] flex-col justify-between bg-[#002512] md:w-[22%] md:translate-x-0`}
+          >
+            <div className="p-8">
+              <div className="flex items-center justify-center">
+                <img src={manimg} className="w-20" alt="" />
+              </div>
+              <div className="flex flex-col place-items-center gap-4 pt-10 text-white uppercase">
+                <NavLink
+                  onClick={() => setToggle(!toggle)}
+                  to="/"
+                  className="flex items-center gap-3 px-6 py-3"
+                >
+                  <p className="text-lg">Home</p>
+                </NavLink>
+                <NavLink
+                  onClick={() => setToggle(!toggle)}
+                  to="/services"
+                  className="flex items-center gap-3 px-6 py-3"
+                >
+                  <p className="text-lg">Services</p>
+                </NavLink>
+                <NavLink
+                  onClick={() => setToggle(!toggle)}
+                  to="/projects"
+                  className="flex items-center gap-3 px-6 py-3"
+                >
+                  <p className="text-lg">Projects</p>
+                </NavLink>
+                <NavLink
+                  onClick={() => setToggle(!toggle)}
+                  to="/contact"
+                  className="flex items-center gap-3 px-6 py-3"
+                >
+                  <p className="text-lg">Contact</p>
+                </NavLink>
+              </div>
             </div>
-            <button className='bg-green-500   px-1 py-4 flex justify-center gap-4 font-semibold md:m-4 m-0 my-8 text-white'>Download CV <FaDownload /></button>
-        </div>
-    )
+            <a className="m-0 my-8 flex justify-center gap-4 bg-green-500 px-1 py-4 font-semibold text-white md:m-4">
+              Download CV <FaDownload />
+            </a>
+          </div>
+        }
+      </div>
+    </div>
+  )
 }
 
 export default Sidebar
